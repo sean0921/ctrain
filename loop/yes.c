2abd0ac4880107eb106936a4fbc937e9b3f1c2cd
sean0921 <sean.li.shin.ho@gmail.com>
2018-03-06 15:05:43 +0800

    test infinite loop

---
+++ loop/yes.c
@@ -0,0 +1,15 @@
+/* yes yes yes yes yes */
+#include <stdio.h>
+
+int main(void)
+{
+    int k = 5566;  /* just specified the random number I want */
+
+    while (k>0)
+    {
+        printf("y\n");
+	k++;
+    }
+
+    return 0;
+}
