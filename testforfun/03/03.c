3d1b08b99036fa9c4adf230d5f88c61de7ac5452
sean0921 <sean.li.shin.ho@gmail.com>
2018-10-21 04:02:26 +0800

    inplement trans 32/64 bit pattern to integer

--- testforfun/03/03.c
+++ testforfun/03/03.c
@@ -10,26 +10,27 @@ int main(int argc, char *argv[]) {
     int trans_mode; // mode, M=1~6
     int i,j;
     int thtw[32];
+    int sxfr[32];
+    int first_int;
     trans_mode = atoi(argv[1]);
 
     switch( trans_mode ) {
         case 1:
+/*
             for(i=0;i<32;i++) {
                 printf("%c",argv[2][i]);
                 thtw[i]=argv[2][i]-'0';
             }
-
+*/
             printf("\n");
             printf("interger: ");
 
-            int a;
-
             for(i=0;i<32;i++) {
-                //printf("%d",thtw[i]);
-                
+                first_int += thtw[i] << (31-i);    
             }
-           
+            printf("%d", first_int);
             printf("\n");
+
             break;
         case 2:
             printf("case 2\n");
@@ -38,7 +39,20 @@ int main(int argc, char *argv[]) {
             printf("case 3\n");
             break;
         case 4:
-            printf("case 4\n");
+/*
+            for(i=0;i<64;i++) {
+                printf("%c",argv[2][i]);
+                sxfr[i]=argv[2][i]-'0';
+            }
+            printf("\n");
+*/
+            printf("interger: ");
+
+            for(i=0;i<64;i++) {
+                first_int += sxfr[i] << (63-i);    
+            }
+            printf("%d", first_int);
+            printf("\n");
             break;
         case 5:
             printf("case 5\n");
