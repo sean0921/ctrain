a2310a14486792f3e00510b5038e0e87dcd0d215
sean0921 <sean.li.shin.ho@gmail.com>
2018-10-20 22:35:45 +0800

    forfun 03: add trans mode via atoi

--- testforfun/03/03.c
+++ testforfun/03/03.c
@@ -1,12 +1,34 @@
 #include <stdio.h>
-#include <stdlib.h>
+#include <stdlib.h> // atoi(), 
 
-int main(int argc, char *argv[])
-{
-    if (argc != 3)
-    {
+int main(int argc, char *argv[]) {
+    if (argc != 3) {
         printf("\x1b[1;31mPlease input at least 2 interger as arguments. :)\x1b[0m\n");
         return 0;
+    }
+
+    int trans_mode;
+    trans_mode = atoi(argv[1]);
+
+    switch( trans_mode ) {
+        case 1:
+            printf("case 1\n");
+            break;	  
+        case 2:
+            printf("case 2\n");
+            break;	  
+        case 3:
+            printf("case 3\n");
+            break;	  
+        case 4:
+            printf("case 4\n");
+            break;	  
+        case 5:
+            printf("case 5\n");
+            break;	  
+        case 6:
+            printf("case 6\n");
+            break;	  
     }
 
     return 0;
