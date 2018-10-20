cbda74285ed04913952c8a5797fb78f5188c089d
sean0921 <sean.li.shin.ho@gmail.com>
2018-10-21 05:12:26 +0800

    basic: add another testing for pointer

--- basic/pointer_trans.c
+++ basic/pointer_trans.c
@@ -4,6 +4,10 @@ int main(void) {
     float a = 111;
     int *x;
     x = &a;
-    printf("test %d %d\n",x, *x);
+    int* x2 = &a;
+    int y = (int)(a);
+    printf("float value = %f, address = %d, integer value = %d\n",a, x, *x);
+    printf("float value = %f, address = %d, integer value = %d\n",a, x2, *x2);
+    printf("float value = %f, address = %d, integer value = %d\n",a, &y, y);
     return 0;
 }
