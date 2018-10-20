853ff8260b3962ccdb96f8608dde8966624db0ee
r2 <r2@ccnsdebian.lan>
2018-10-20 22:15:54 +0800

    forfun 03: Initial testing

---
+++ testforfun/03/03.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[])
+{
+    if (argc != 3)
+    {
+        printf("\x1b[1;31mPlease input at least 2 interger as arguments. :)\x1b[0m\n");
+        return 0;
+    }
+
+    return 0;
+}
+
