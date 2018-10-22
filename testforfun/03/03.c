1689d1dca9f8d93d87578ce7e9ac9746adc85574
sean0921 <sean.li.shin.ho@gmail.com>
2018-10-22 22:37:25 +0800

    forfun: 03: correct wrong declaration of sxfr
    and remove debug define macro
    fix some errors.

--- testforfun/03/03.c
+++ testforfun/03/03.c
@@ -6,22 +6,23 @@
 
 int main(int argc, char *argv[]) {
     if (argc != 3) {
-        printf("\x1b[1;31mPlease input at least 2 interger as arguments. :)\x1b[0m\n");
+        printf("Please input at least 2 interger as arguments. :)\x1b[0m\n");
         return 0;
     }
 
     int trans_mode; // mode, M=1~6
     int i,j;
     int thtw[32];
-    int sxfr[64];
+    unsigned long long sxfr[64];
     int sign;
     int exponent_orig = -127;
     float exponent = 1;
     int mantisa_orig[23];
-    float mantisa_orig_flt[23];
-    float mantisa_flt;
-    int result_int = 0;
-    float result_flt;
+    float mantisa_orig_float[23];
+    float mantisa_float;
+    unsigned int result_int = 0;
+    unsigned long long result_int64 = 0;
+    float result_float;
     int32_t input_int32;
     int64_t input_int64;
     float input_float;
@@ -39,59 +40,9 @@ int main(int argc, char *argv[]) {
                 result_int += thtw[i] << (31-i);
             }
             printf("interger: %d\n", result_int);
-
-            /* calculate float */
-            /* calculate exponent part */
-#ifdef DEBUG
-            printf("\x1b[1;31mexponent part: \x1b[m");
-#endif
-            for(i=0;i<8;i++) {
-                exponent_orig +=  (thtw[i+1] << (7-i));
-#ifdef DEBUG
-                printf("\x1b[1;31m%d\x1b[m",thtw[i+1]);
-#endif
-            }
-#ifdef DEBUG
-            printf("\n");
-            printf("\x1b[1;31mexponent_orig: %d \x1b[m\n",exponent_orig);
-#endif
-
-            if(exponent_orig<0) {
-                exponent = 1 << ( 0 - exponent_orig );
-                exponent = 1 / ( (float) exponent );
-            } else {
-                exponent = (float) (1 << exponent_orig);
-            }
-
-#ifdef DEBUG
-            printf("\x1b[1;31mexponent: %f \n\x1b[m",exponent);
-#endif
-
-            for(i=0;i<23;i++) {
-                mantisa_orig[i] =  thtw[i+9] << (i+1);
-                if(mantisa_orig[i]!=0) {
-                    mantisa_orig_flt[i] = 1 / (float)(mantisa_orig[i]);
-                } else {
-                    mantisa_orig_flt[i] = 0;
-                }
-#ifdef DEBUG
-                printf("\x1b[1;31m%f \x1b[m",mantisa_orig_flt[i]);
-#endif
-                mantisa_flt += mantisa_orig_flt[i];
-            }
-#ifdef DEBUG
-            printf("\n\x1b[1;31mmantisa_flt = %f\x1b[m \n", mantisa_flt);
-#endif
-
-            if(thtw[0]==1){
-                sign = -1;
-            } else {
-                sign = 1;
-            }
-
-            result_flt = ( 1.000 + mantisa_flt ) * exponent * sign;
-            printf("float: %f \n", result_flt);
-
+            float trans_bits_float;
+            trans_bits_float = *((float*)&result_int);
+            printf("float: %f\n", trans_bits_float);
             break;
 
         case 2:
@@ -126,67 +77,18 @@ int main(int argc, char *argv[]) {
             /* calculate decimal integer first */
             for(i=0;i<64;i++) {
                 sxfr[i]=argv[2][i]-'0';
-                result_int += sxfr[i] << (63-i);
+                result_int64 += sxfr[i] << (63-i);
             }
-            printf("interger: %d\n", result_int);
-
-            /* calculate float */
-            /* calculate exponent part */
-#ifdef DEBUG
-            printf("\x1b[1;31mexponent part: \x1b[m");
-#endif
-            for(i=0;i<8;i++) {
-                exponent_orig +=  (sxfr[i+1] << (7-i));
-#ifdef DEBUG
-                printf("\x1b[1;31m%d\x1b[m",sxfr[i+1]);
-#endif
-            }
-#ifdef DEBUG
-            printf("\n");
-            printf("\x1b[1;31mexponent_orig: %d \x1b[m\n",exponent_orig);
-#endif
-
-            if(exponent_orig<0) {
-                exponent = 1 << ( 0 - exponent_orig );
-                exponent = 1 / ( (float) exponent );
-            } else {
-                exponent = (float) (1 << exponent_orig);
-            }
-
-#ifdef DEBUG
-            printf("\x1b[1;31mexponent: %f \n\x1b[m",exponent);
-#endif
-
-            for(i=0;i<55;i++) {
-                mantisa_orig[i] =  sxfr[i+9] << (i+1);
-                if(mantisa_orig[i]!=0) {
-                    mantisa_orig_flt[i] = 1 / (float)(mantisa_orig[i]);
-                } else {
-                    mantisa_orig_flt[i] = 0;
-                }
-#ifdef DEBUG
-                printf("\x1b[1;31m%f \x1b[m",mantisa_orig_flt[i]);
-#endif
-                mantisa_flt += mantisa_orig_flt[i];
-            }
-#ifdef DEBUG
-            printf("\n\x1b[1;31mmantisa_flt = %f\x1b[m \n", mantisa_flt);
-#endif
-
-            if(sxfr[0]==1){
-                sign = -1;
-            } else {
-                sign = 1;
-            }
-
-            result_flt = ( 1.000 + mantisa_flt ) * exponent * sign;
-            printf("float: %f \n", result_flt);
+            printf("interger: %llu\n", result_int64);
+            double trans_bits_double;
+            trans_bits_double = *((double*)&result_int64);
+            printf("double: %lf\n", trans_bits_double);
             break;
 
         case 5:
             input_int64 = atoi(argv[2]);
             printf("64-bit: ");
-            for(i=0;i<63;i++) {
+            for(i=0;i<64;i++) {
                 j = input_int64 >> (63-i);
                 if(j & 1) {
                     printf("1");
@@ -197,10 +99,10 @@ int main(int argc, char *argv[]) {
             printf("\n");
             break;
         case 6:
-            input_float = atof(argv[2]);
+            input_double = atof(argv[2]);
             printf("64-bit: ");
-            int64_t trans_bits64;
-            trans_bits64 = *((int64_t*)&input_float);
+            unsigned long long trans_bits64;
+            trans_bits64 = *((unsigned long long*)&input_double);
             for(i=0;i<64;i++) {
                 j = trans_bits64 >> (63-i);
                 if(j & 1) {
