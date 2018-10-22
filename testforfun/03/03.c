c39c4bc3227fd022fcb6927f931700c3a3285e64
sean0921 <sean.li.shin.ho@gmail.com>
2018-10-22 13:21:41 +0800

    forfun: 03: implement case 3 6, clone case 1 code to case 4

--- testforfun/03/03.c
+++ testforfun/03/03.c
@@ -14,6 +14,7 @@ int main(int argc, char *argv[]) {
     int i,j;
     int thtw[32];
     int sxfr[64];
+    int sign;
     int exponent_orig = -127;
     float exponent = 1;
     int mantisa_orig[23];
@@ -23,6 +24,8 @@ int main(int argc, char *argv[]) {
     float result_flt;
     int32_t input_int32;
     int64_t input_int64;
+    float input_float;
+    double input_double;
     int result_thtw[32];
     int result_sxfr[64];
     
@@ -80,7 +83,6 @@ int main(int argc, char *argv[]) {
             printf("\n\x1b[1;31mmantisa_flt = %f\x1b[m \n", mantisa_flt);
 #endif
 
-            int sign;
             if(thtw[0]==1){
                 sign = -1;
             } else {
@@ -106,20 +108,81 @@ int main(int argc, char *argv[]) {
             printf("\n");
             break;
         case 3:
-            printf("case 3\n");
-            break;
-        case 4:
-            for(i=0;i<64;i++) {
-                sxfr[i]=argv[2][i]-'0';
+            input_float = atof(argv[2]);
+            printf("32-bit: ");
+            int32_t trans_bits;
+            trans_bits = *((int32_t*)&input_float);
+            for(i=0;i<32;i++) {
+                j = trans_bits >> (31-i);
+                if(j & 1) {
+                    printf("1");
+                } else {
+                    printf("0");
+                }
             }
-            printf("interger: ");
-
-            for(i=0;i<64;i++) {
-                result_int += sxfr[i] << (63-i);
-            }
-            printf("%d", result_int);
             printf("\n");
             break;
+        case 4:
+            /* calculate decimal integer first */
+            for(i=0;i<64;i++) {
+                sxfr[i]=argv[2][i]-'0';
+                result_int += sxfr[i] << (63-i);
+            }
+            printf("interger: %d\n", result_int);
+
+            /* calculate float */
+            /* calculate exponent part */
+#ifdef DEBUG
+            printf("\x1b[1;31mexponent part: \x1b[m");
+#endif
+            for(i=0;i<8;i++) {
+                exponent_orig +=  (sxfr[i+1] << (7-i));
+#ifdef DEBUG
+                printf("\x1b[1;31m%d\x1b[m",sxfr[i+1]);
+#endif
+            }
+#ifdef DEBUG
+            printf("\n");
+            printf("\x1b[1;31mexponent_orig: %d \x1b[m\n",exponent_orig);
+#endif
+
+            if(exponent_orig<0) {
+                exponent = 1 << ( 0 - exponent_orig );
+                exponent = 1 / ( (float) exponent );
+            } else {
+                exponent = (float) (1 << exponent_orig);
+            }
+
+#ifdef DEBUG
+            printf("\x1b[1;31mexponent: %f \n\x1b[m",exponent);
+#endif
+
+            for(i=0;i<55;i++) {
+                mantisa_orig[i] =  sxfr[i+9] << (i+1);
+                if(mantisa_orig[i]!=0) {
+                    mantisa_orig_flt[i] = 1 / (float)(mantisa_orig[i]);
+                } else {
+                    mantisa_orig_flt[i] = 0;
+                }
+#ifdef DEBUG
+                printf("\x1b[1;31m%f \x1b[m",mantisa_orig_flt[i]);
+#endif
+                mantisa_flt += mantisa_orig_flt[i];
+            }
+#ifdef DEBUG
+            printf("\n\x1b[1;31mmantisa_flt = %f\x1b[m \n", mantisa_flt);
+#endif
+
+            if(sxfr[0]==1){
+                sign = -1;
+            } else {
+                sign = 1;
+            }
+
+            result_flt = ( 1.000 + mantisa_flt ) * exponent * sign;
+            printf("float: %f \n", result_flt);
+            break;
+
         case 5:
             input_int64 = atoi(argv[2]);
             printf("64-bit: ");
@@ -134,7 +197,19 @@ int main(int argc, char *argv[]) {
             printf("\n");
             break;
         case 6:
-            printf("case 6\n");
+            input_float = atof(argv[2]);
+            printf("64-bit: ");
+            int64_t trans_bits64;
+            trans_bits64 = *((int64_t*)&input_float);
+            for(i=0;i<64;i++) {
+                j = trans_bits64 >> (63-i);
+                if(j & 1) {
+                    printf("1");
+                } else {
+                    printf("0");
+                }
+            }
+            printf("\n");
             break;
     }
 
