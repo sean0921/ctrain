9ed719d406526a02f39a269a5f643814f115a2db
sean0921 <sean.li.shin.ho@gmail.com>
2018-10-28 20:06:47 +0800

    forfun: 04: add check_legal(), preparing for another function

--- testforfun/04/4-2.c
+++ testforfun/04/4-2.c
@@ -6,10 +6,11 @@
 
 #include <stdio.h>
 #include <stdlib.h>
-#include <unistd.h>  //for sleep() function
 
 #define ANSI_RED    "\x1b[1;31m"
 #define ANSI_RESET  "\x1b[0m"
+
+int i;
 
 // usage: int check_argument(<number>)
 // use it to specific
@@ -22,16 +23,47 @@ int check_arguments(int argnum, int num) {
     }
 }
 
+int check_legal(int array[], int array_number, int max_integer, int legal_label) 
+{
+    for(i=0;i<array_number;i++) {
+        if(array[i]>0) {
+            if(array[i] <= max_integer) legal_label=1; 
+            else {
+                legal_label=0;
+                break;
+            }
+        } else {
+            legal_label=0;
+            break;
+        }
+    }
+    return legal_label;
+}
+
+#if 0
+int compare_match(int* array1[], int* array2[], int* ish[], int* isx[], int array_position) {
+        for(i=0;i<array_position;i++) {
+            if(array1[i]==array2[i]) {
+                h++;
+                ish[i]=1;
+                isx[i]=0;
+            } else {
+                ish[i]=0;
+                isx[i]=0;
+            }
+        }
+#endif
+
 int main(int argc, char *argv[])
 {
+
+    int maxi, nop; 
+    int legal=0; //max interger, number of position, if it is legal (1: true, 0: false)
+    int arr[nop];
 
     if( check_arguments(argc,2) != 0) {
         return -1;
     }
-
-    int maxi, nop, legal=0; //max interger, number of position, if it is legal (1: true, 0: false)
-    int arr[nop];
-    int i;
 
 /* using argc argv and "a to i" ways */
     maxi = atoi(argv[1]);
@@ -39,173 +71,80 @@ int main(int argc, char *argv[])
 
     printf("You Set N = %d , P = %d \n", maxi, nop);
 
-while(1)
-{
-    printf("ans: ");
-    for(i=0;i<nop;i++)
+    while(1)
     {
-        scanf("%d",&arr[i]);
+        printf("ans: ");
+        for(i=0;i<nop;i++) {
+            scanf("%d",&arr[i]);
+        }
+    
+        legal = check_legal(arr,nop,maxi,legal);
+    
+        if(legal==1) {
+            break;
+        } else {
+            printf("The answer is illegal, please reset the answer.\n");
+        }
     }
-
-    for(i=0;i<nop;i++)
+    
+    int gus[nop];
+    legal=0; //reset legal value for prepare checking next array
+    
+    while(1)
     {
-        if(arr[i]>0)
-        {
-            if(arr[i]<=maxi)
-            {
-                legal=1;
-#ifdef DEBUG
-                printf("legal: %d\n",legal);
-                sleep(1);
+        printf("guess: ");
+        for(i=0;i<nop;i++) {
+            scanf("%d",&gus[i]);
+        }
+    
+        legal = check_legal(gus,nop,maxi,legal);
+    
+        if(legal==0) {
+            printf("The answer is illegal, please reset the answer.\n");
+            continue;
+        }
+    
+        int h=0, x=0, ish[nop], isx[nop];
+        int j;
+    
+//        compare_match(int array1[], int array2[], int array_position) {
+
+#if 0
+            int b;
+            int a[10];
+            a[0];
+            *(a+0);
+            a[1];
+            (a+1)=&b;
 #endif
-            }
-            else
-            {
-                legal=0;
-#ifdef DEBUG
-                printf("legal: %d\n",legal);
-                sleep(1);
-#endif
-                break;
+
+//        h = compare_match_value(arr,gus,&ish,&isx,nop);
+        for(i=0;i<nop;i++) {
+            if(gus[i]==arr[i]) {
+                h++;
+                ish[i]=1;
+                isx[i]=0;
+            } else {
+                ish[i]=0;
+                isx[i]=0;
             }
         }
-        else
-        {
-            legal=0;
-#ifdef DEBUG
-            printf("legal: %d\n",legal);
-            sleep(1);
-#endif
+    
+//        x = compare_nomatch_value(arr,gus,&ish,&isx,nop);
+        for(i=0;i<nop;i++) {
+            for(j=0;j<nop;j++) {
+                if(gus[i]==arr[j] && ish[i]==0 && isx[i]==0 && ish[j]==0 && isx[j]==0 ) {
+                    x++;
+                    isx[j]=1;
+                }
+            }
+        }
+    
+        printf("%dH %dX \n",h,x);
+        if(h==nop) { 
             break;
         }
     }
-
-#ifdef DEBUG
-    printf("so legel: %d\n",legal);
-    sleep(1);
-#endif
-
-    if(legal==1)
-    {
-        break;
-    }
-    else
-    {
-        printf("The answer is illegal, please reset the answer.\n");
-        fflush(stdin);
-    }
-}
-
-#ifdef DEBUG
-    printf("ok! ans: ");
-    for(i=0;i<nop;i++)
-        printf("%d ",arr[i]);
-        sleep(1);
-    printf("\n");
-#endif
-
-int gus[nop];
-legal=0; //reset legal value for prepare checking next array
-
-while(1)
-{
-    printf("guess: ");
-    for(i=0;i<nop;i++)
-    {
-        scanf("%d",&gus[i]);
-    }
-
-    for(i=0;i<nop;i++)
-    {
-        if(gus[i]>0)
-        {
-            if(gus[i]<=maxi)
-            {
-                legal=1;
-#ifdef DEBUG
-                printf("legal: %d\n",legal);
-                sleep(1);
-#endif
-            }
-            else
-            {
-                legal=0;
-#ifdef DEBUG
-                printf("legal: %d\n",legal);
-                sleep(1);
-#endif
-                break;
-            }
-        }
-        else
-        {
-            legal=0;
-#ifdef DEBUG
-            printf("legal: %d\n",legal);
-            sleep(1);
-#endif
-            break;
-        }
-    }
-
-#ifdef DEBUG
-    printf("so legel: %d\n",legal);
-#endif
-
-    if(legal==0)
-    {
-        printf("The answer is illegal, please reset the answer.\n");
-        continue;
-    }
-
-    int h=0, x=0, ish[nop], isx[nop];
-    int j;
-
-    for(i=0;i<nop;i++)
-    {
-        if(gus[i]==arr[i])
-        {
-            h++;
-            ish[i]=1;
-            isx[i]=0;
-        }
-        else
-        {
-            ish[i]=0;
-            isx[i]=0;
-        }
-    }
-
-    for(i=0;i<nop;i++)
-    {
-        for(j=0;j<nop;j++)
-        {
-#ifdef DEBUG
-            printf("gus[%d]=%d, arr[%d]=%d, ish[%d]=%d, isx[%d]=%d.\n",i, gus[i], j, arr[j], j, ish[j], j, isx[j]);
-            sleep(1);
-#endif
-            if(gus[i]==arr[j] && ish[i]==0 && isx[i]==0 && ish[j]==0 && isx[j]==0 )
-            {
-                x++;
-                isx[j]=1;
-            }
-        }
-    }
-
-    printf("%dH %dX \n",h,x);
-#ifdef DEBUG
-    printf("well, ans: ");
-    for(i=0;i<nop;i++)
-        printf("%d ",arr[i]);
-        sleep(1);
-    printf("\n");
-#endif
-
-    if(h==nop)
-    { 
-        break;
-    }
-}
 
     return 0;
 }
